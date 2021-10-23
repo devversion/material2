4fa28c385fc8dcfbaccbb7a89013178a48f00e2e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-23 16:37:49 +0200

    ds

--- scripts/deploy-dev-app.js
+++ scripts/deploy-dev-app.js
@@ -5,42 +5,98 @@
  * deployed to the currently configured Firebase project.
  */
 
-const {exec, set, cd, cp, rm, chmod} = require('shelljs');
+const {exec, cd, cp, rm, chmod} = require('shelljs');
 const {join} = require('path');
+const minimist = require('minimist');
 
-// ShellJS should throw if any command fails.
-set('-e');
+main().catch(e => {
+  console.error(e);
+  process.exitCode = 1
+});
 
-/** Path to the project directory. */
-const projectDirPath = join(__dirname, '../');
+async function main() {
+  /** Parsed command line arguments. */
+  const args = minimist(process.argv.slice(2),
+    {string: ['channelId', 'project'], boolean: ['json']});
 
-// Go to project directory.
-cd(projectDirPath);
+  /** Whether this script is expected to output the deployment info as JSON. */
+  const outputJson = args.json;
 
-/** Path to the bazel-bin directory. */
-const bazelBinPath = exec(`yarn -s bazel info bazel-bin`).stdout.trim();
+  /**
+   * Firebase hosting channel where the dev-app should be deployed to.
+   * Deploys to the "live" channel by default.
+   * https://firebase.google.com/docs/hosting/manage-hosting-resources.
+   */
+  const channelId = args.channelId ?? 'live';
 
-/** Output path for the Bazel dev-app web package target. */
-const webPackagePath = join(bazelBinPath, 'src/dev-app/web_package');
+  /**
+   * Project into which the dev-app should be deployed. By default, the previously
+   * selected project is used, or the `.firebaserc` file is consulted.
+   */
+  const projectId = args.project;
 
-/** Destination path where the web package should be copied to. */
-const distPath = join(projectDirPath, 'dist/dev-app-web-pkg');
+  /** Path to the project directory. */
+  const projectDirPath = join(__dirname, '../');
 
-// Build web package output.
-exec('yarn -s bazel build //src/dev-app:web_package');
+  // Go to project directory.
+  cd(projectDirPath);
 
-// Clear previous deployment artifacts.
-rm('-Rf', distPath);
+  /** Path to the bazel-bin directory. */
+  const bazelBinPath = (await silentExec(`bazel info bazel-bin`)).trim();
 
-// Copy the web package from the bazel-bin directory to the project dist
-// path. This is necessary because the Firebase CLI does not support deployment
-// of a public folder outside of the "firebase.json" file.
-cp('-R', webPackagePath, distPath);
+  /** Output path for the Bazel dev-app web package target. */
+  const webPackagePath = join(bazelBinPath, 'src/dev-app/web_package');
 
-// Bazel by default marks output files as `readonly` to ensure hermeticity. Since we moved
-// these files out of the `bazel-bin` directory, we should make them writable. This is necessary
-// so that subsequent runs of this script can delete old contents from the deployment dist folder.
-chmod('-R', 'u+w', distPath);
+  /** Destination path where the web package should be copied to. */
+  const distPath = join(projectDirPath, 'dist/dev-app-web-pkg');
 
-// Run the Firebase CLI to deploy the hosting target.
-exec(`yarn -s firebase deploy --only hosting`);
+  // Build web package output.
+  await silentExec('bazel build //src/dev-app:web_package');
+
+  // Clear previous deployment artifacts.
+  rm('-Rf', distPath);
+
+  // Copy the web package from the bazel-bin directory to the project dist
+  // path. This is necessary because the Firebase CLI does not support deployment
+  // of a public folder outside of the "firebase.json" file.
+  cp('-R', webPackagePath, distPath);
+
+  // Bazel by default marks output files as `readonly` to ensure hermeticity. Since we moved
+  // these files out of the `bazel-bin` directory, we should make them writable. This is necessary
+  // so that subsequent runs of this script can delete old contents from the deployment dist folder.
+  chmod('-R', 'u+w', distPath);
+
+  // If an explicit project is specified, pass it to the deploy command.
+  const projectIdArg = projectId ? `--project ${projectId}` : '';
+  const jsonArg = outputJson ? `--json` : '';
+  const deployCommandArg = channelId === 'live' ?
+    'deploy --only hosting' : `hosting:channel:deploy ${channelId}`;
+
+  // Run the Firebase CLI to deploy the hosting target.
+  const deployResultStdout = await silentExec(
+    `yarn -s firebase ${deployCommandArg} ${projectIdArg} ${jsonArg}`);
+
+  if (outputJson) {
+    process.stdout.write(deployResultStdout.trim());
+  }
+}
+
+/**
+ * Executes the given command silently without polluting the stdout of the current process.
+ * This is necessary so that the JSON output is not colliding with other output.
+ * @returns a promise resolving with the command stdout on success.
+ */
+function silentExec(command) {
+  return new Promise((resolve, reject) => {
+    const execProcess = exec(command, {silent: true, fatal: false, async: true});
+    let stdout = '';
+
+    execProcess.stdout.on('data', value => {
+      stdout += value;
+      process.stderr.write(value)
+    });
+
+    execProcess.stderr.on('data', value => process.stderr.write(value));
+    execProcess.on('close', status => status === 0 ? resolve(stdout) : reject());
+  });
+}
